4dacd63eb4214b6df492b051a7aa22c8eabb9446
Arthur Augusto <arthuraasantos@hotmail.com>
2016-12-19 23:29:29 -0200

    Arquivos pendentes

---
+++ login/controllers/loginCtrl.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    angular.module('sexmoveApp')
+        .controller('LoginCtrl', function ($rootScope) {
+            $rootScope.appTitle = "Sex Move";
+        })
+        .config(['$routeProvider', '$locationProvider', function (routeProvider, locationProvider) {
+
+
+            locationProvider.html5Mode(true);
+
+            routeProvider.when('/', {
+                templateUrl: 'login/templates/index.html',
+                controller: 'LoginCtrl',
+                name: 'login'
+            }).otherwise({ redirectTo: "/" });
+
+            routeProvider.when('/newRegister', {
+                templateUrl: '../../register/templates/register.html',
+                controller: 'LoginCtrl',
+                name: 'newRegister'
+            });
+        }]);
+
+
+
+
+})();    
