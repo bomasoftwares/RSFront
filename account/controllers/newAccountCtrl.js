7db467f76c7e952152dc9dcd0a601e15e04cd730
Arthur Augusto <arthuraasantos@hotmail.com>
2017-07-09 11:23:47 -0300

    Redirecionar usuário após criação de conta para dentro do sistema

--- account/controllers/newAccountCtrl.js
+++ account/controllers/newAccountCtrl.js
@@ -22,22 +22,15 @@
                     "&username="+convertToFormUrlEncoded(nickName)+
                     "&password="+convertToFormUrlEncoded(password);
                     
-                    return 
-                    Restangular
-                        .allUrl("token",Restangular.configuration.baseUrl+"/token")
-                        .customPOST(content,"",{},{ 'Content-Type': 'application/x-www-form-urlencoded'})
-                        .then(function(result){
-                            tokenFactory.setToken(result.access_token);
-                        })
-                        .catch(function(responseError){
-                            console.log('erro token' + responseError)
-                        });
+                    return Restangular.allUrl("token",Restangular.configuration.baseUrl+"/token")
+                        .customPOST(content,"",{},{ 'Content-Type': 'application/x-www-form-urlencoded'});
+                       
             }
 
             var service = {
 
                 getToken: function (nickName, password) {
-                    _getToken(nickName, password);
+                    return _getToken(nickName, password);
                 },
                 post: function(Model){
                     return Restangular.all("users").post(Model);
@@ -53,8 +46,8 @@
 
             return service;
         }])
-    .controller('newAccountCtrl',['$scope','Restangular', 'newAccountService', 'messageFactory', 'tokenFactory',  
-    function ($scope, Restangular, newAccountService, messageFactory, tokenFactory) {
+    .controller('newAccountCtrl',['$scope','Restangular', 'newAccountService', 'messageFactory', 'tokenFactory', '$window',
+    function ($scope, Restangular, newAccountService, messageFactory, tokenFactory, $window) {
         
         function initializer(){
             Restangular.setBaseUrl('http://dev-sexmoveapi.boma.com.br');
@@ -68,18 +61,33 @@
              if (form && form.$valid) {
 
                 newAccountService.post($scope.Model).then(function(response){
-                    var token = tokenFactory.getToken();
 
-                    if(token){
-                        messageFactory.addSuccessMessage('Conta criada com sucesso');
+                    newAccountService.getToken($scope.Model.NickName, $scope.Model.Password).then(function(responseToken){
+                        tokenFactory.setToken(responseToken.access_token);
+                        var token = tokenFactory.getToken();
+
+                        if(token) $window.location = "/index.html";
+                        else 
+                            messageFactory.addErrorMessage('Erro ao fazer login');
+
                         $scope.isLoading = false;
-                    }
+                    }).catch(function(responseError){
+                        console.log(responseError);
+                        messageFactory.addErrorMessage('Ocorreu um erro ao fazer login');
+                        $scope.isLoading = false;
+                    });
                 })
-                .catch(function(response){
-                    if(response.status == "400" || response.message != undefined)
-                        messageFactory.addErrorMessage('Não foi possível criar sua conta');
+                .catch(function(responseError){
 
-                        $scope.isLoading = false;
+                    if(responseError.data.ErrorDescription != undefined && responseError.data.ErrorDescription.length <= 50)
+                        messageFactory.addErrorMessage(responseError.data.ErrorDescription)
+                    else 
+                    {
+                        console.log(responseError);
+                        messageFactory.addErrorMessage('Não foi possível criar sua conta');
+                    }
+
+                    $scope.isLoading = false;
                 });
              }
         }
