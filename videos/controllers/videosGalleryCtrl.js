0170073db939b078994dd56e8c1ad9cff76e185a
Arthur Augusto <arthuraasantos@hotmail.com>
2017-08-09 21:36:19 -0300

    Módulo de fotos e removendo links do menu de usuários

--- videos/controllers/videosGalleryCtrl.js
+++ videos/controllers/videosGalleryCtrl.js
@@ -5,72 +5,54 @@
         function ($scope,videosService, Upload, tokenFactory) {
         var vm = this;
         vm.isLoadingGallery = false;
-       
+        vm.videos = [];
 
         //Método de inicialização
         function init(){
-            //getPhotos();
+            getVideos();
         }
 
-        // function getPhotos(){
-        //     vm.isLoadingGallery = true;
-        //     photosService.getPhotos().then(function(response){
-        //         vm.photos = response;
-        //         vm.isLoadingGallery = false;
-        //     }).catch(function(responseError){
-        //         console.log(responseError);
-        //         vm.isLoadingGallery = false;
-        //     });
-        // }
+        function getVideos(){
+            vm.isLoadingGallery = true;
+            videosService.getVideos().then(function(response){
+                vm.videos = response;
+                vm.isLoadingGallery = false;
+            }).catch(function(responseError){
+                console.log(responseError);
+                vm.isLoadingGallery = false;
+            });
+        }
 
-        // vm.deletePhoto = function(photoId)
-        // {
-        //     vm.isLoadingGallery = true;
-        //     photosService.deletePhoto(photoId).then(function(response){
-        //         var index = vm.photos.map(function(c){ return c.Id}).indexOf(photoId);
-        //         vm.photos.splice(index,1);
-        //         vm.isLoadingGallery = false;
-        //     }).catch(function(responseError){
-        //         console.log(responseError);
-        //         vm.isLoadingGallery = false;
+        vm.deleteVideo = function(videoId)
+        {
+            vm.isLoadingGallery = true;
+            videosService.deleteVideo(videoId).then(function(response){
+                var index = vm.videos.map(function(c){ return c.Id}).indexOf(videoId);
+                vm.videos.splice(index,1);
+                vm.isLoadingGallery = false;
+            }).catch(function(responseError){
+                console.log(responseError);
+                vm.isLoadingGallery = false;
 
-        //     });
-        // }
+            });
+        }
 
-        // vm.onFileSelect = function($files) {
-        //     vm.isLoading = true;
-        //     Upload.upload({
-        //         url: 'http://dev-sexmoveapi.boma.com.br/files',
-        //         file: $files,
-        //         headers: {
-        //             'X-Requested-With': 'XMLHttpRequest',
-        //             "Authorization": 'Bearer'+ tokenFactory.getToken()
-        //         }            
-        //     }).progress(function(e) {
-        //     }).then(function(data, status, headers, config) {
-        //         getPhotos();
-        //         vm.isLoading = false;
-        //     }); 
-        // }
-
-        // vm.uploadFile = function(formNewPhoto){
-
-        //     if(formNewPhoto.file.$valid)
-        //     {
-        //         Upload.upload({
-        //             url: 'http://dev-sexmoveapi.boma.com.br' + '/files',
-        //             data: {file: formNewPhoto.file},
-        //             headers: {
-        //             'X-Requested-With': 'XMLHttpRequest',
-        //             "Authorization": 'Bearer'+ tokenFactory.getToken()
-        //             }
-        //         }).then(function(response){
-        //             vm.getPhotos();
-        //         }).catch(function(responseError){
-        //             vm.UploadPhotoErrorMessage = "Erro ao inserir imagem";
-        //         });
-        //     }
-        // }
+        vm.onFileSelect = function($files) {
+            vm.isLoadingGallery = true;
+            Upload.upload({
+                url: 'http://dev-sexmoveapi.boma.com.br/files',
+                file: $files,
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    "Authorization": 'Bearer'+ tokenFactory.getToken()
+                }            
+            }).progress(function(e) {
+            }).then(function(data, status, headers, config) {
+                vm.isLoadingGallery = true;
+                getVideos();
+                vm.isLoading = false;
+            }); 
+        }
 
         init();
     }]);
