9914cae7ff53883d3c2a82e27c53022067bc38fe
Arthur Augusto <arthuraasantos@hotmail.com>
2017-08-10 00:46:47 -0300

    Relatório de fotos e vídeos

--- user/controllers/userHomeCtrl.js
+++ user/controllers/userHomeCtrl.js
@@ -1,16 +1,39 @@
 (function () {
     'use strict';
 
-    angular.module('bmSexMoveApp').controller('userHomeCtrl', function ($scope) {
-
+    angular.module('bmSexMoveApp').controller('userHomeCtrl',['photosService', function (photosService) {
+        var vm = this;
         //Método de inicialização
         function init(){
-            
+            getLatestPhotos();
+            getLatestVideos();
+        }
+
+        function getLatestPhotos(){
+            vm.isLoadingPhotosReport = true;
+            photosService.latestReportPhotos().then(function(response){
+                vm.latestPhotosItems = response;
+                vm.isLoadingPhotosReport = false;
+            }).catch(function(responseError){
+                console.log(responseError);
+                vm.isLoadingPhotosReport = false;
+            });
+        }
+
+        function getLatestVideos(){
+            vm.isLoadingVideosReport = true;
+            photosService.latestReportVideos().then(function(response){
+                vm.latestVideosItems = response;
+                vm.isLoadingVideosReport = false;
+            }).catch(function(responseError){
+                console.log(responseError);
+                vm.isLoadingVideosReport = false;
+            });
         }
 
         
 
         
         init();
-    });
+    }]);
 })();
